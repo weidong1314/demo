966a79ab8abe4250ef0881699f1c86fe754cec37
卫东 <512732559@qq.com>
2020-10-24 15:27:55 +0800

    初始化

---
+++ demo-parent/demo-apply/src/test/java/com/demo/demoapply/DemoApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demoapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
